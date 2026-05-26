545c81186c12fb75fb5dd4d40a256a7f29f0fe69
nicholasli <nicholasli9@qq.com>
2026-05-26 15:31:41 +0800

    bench(toolkit): expand benchmarks with lookup_1000, descriptions, schema_build

--- crates/pawbun-toolkit/benches/toolkit.rs
+++ crates/pawbun-toolkit/benches/toolkit.rs
@@ -1,7 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use pawbun_mcp_core::schema_convert::parameters_to_schema;
 use pawbun_toolkit::{
     Tool, ToolError, ToolExecutor, ToolKit, ToolParameter, ToolRegistry, ToolResult,
 };
+use serde_json::json;
 use std::borrow::Cow;
 
 #[derive(Debug)]
@@ -93,10 +95,62 @@ impl Tool for NamedNoOpTool {
     }
 }
 
+fn benchmark_registry_lookup_1000(c: &mut Criterion) {
+    let mut kit = ToolKit::new();
+    for i in 0..1000 {
+        kit.register(Box::new(NamedNoOpTool(format!("tool_{}", i))));
+    }
+
+    c.bench_function("registry_lookup/1000", |b| {
+        b.iter(|| {
+            let _ = kit.get(black_box("tool_500"));
+        })
+    });
+}
+
+fn benchmark_tool_descriptions(c: &mut Criterion) {
+    let mut kit = ToolKit::new();
+    for i in 0..100 {
+        kit.register(Box::new(NamedNoOpTool(format!("tool_{}", i))));
+    }
+
+    c.bench_function("tool_descriptions/100", |b| {
+        b.iter(|| {
+            let _ = black_box(kit.descriptions());
+        })
+    });
+}
+
+fn benchmark_schema_build(c: &mut Criterion) {
+    let params = vec![
+        ToolParameter {
+            name: "url".into(),
+            description: "URL to fetch".into(),
+            required: true,
+            schema: json!({"type": "string", "format": "uri"}),
+        },
+        ToolParameter {
+            name: "max_length".into(),
+            description: "Max length".into(),
+            required: false,
+            schema: json!({"type": "integer"}),
+        },
+    ];
+    c.bench_function("schema_build/10_params", |b| {
+        b.iter(|| {
+            let schema = parameters_to_schema(black_box(&params));
+            black_box(schema);
+        })
+    });
+}
+
 criterion_group!(
     benches,
     benchmark_registry_lookup,
+    benchmark_registry_lookup_1000,
     benchmark_tool_execution,
-    benchmark_register
+    benchmark_register,
+    benchmark_tool_descriptions,
+    benchmark_schema_build
 );
 criterion_main!(benches);
